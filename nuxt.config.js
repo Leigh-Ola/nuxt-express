fe923f7c35c2055ab94b7fd636b4a31367066677
Leigh-Ola <leolaotan@gmail.com>
2020-10-19 20:44:24 +0100

    Edited

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,6 +51,12 @@ export default {
     port: 3000, // default: 3000
     // host: "0.0.0.0", // default: localhost
   },
+  /**
+   * Environment variabes
+   */
+  env: {
+    baseUrl: process.env.BASE_URL || "http://localhost:3000",
+  },
   /*
    ** Global CSS
    */
